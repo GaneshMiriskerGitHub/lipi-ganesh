fd037151ef6105ec14d6ff2a605e650bb858702a
ganesh mirisker <ganeshmirisker@gmail.com>
2023-11-03 09:23:14 +0530

    ORM project added, need to build this project end to end

---
+++ Lipi_Kosam_Springboot_ORM/src/main/java/com/amazon/LipiKosamSpringbootOrmApplication.java
@@ -0,0 +1,13 @@
+package com.amazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LipiKosamSpringbootOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LipiKosamSpringbootOrmApplication.class, args);
+	}
+
+}
