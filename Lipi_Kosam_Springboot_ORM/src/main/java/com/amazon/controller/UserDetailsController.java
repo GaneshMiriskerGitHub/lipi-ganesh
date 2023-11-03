6dc56895077272b55e3d506ce01e277e12568e61
ganesh mirisker <ganeshmirisker@gmail.com>
2023-11-03 09:41:22 +0530

    chinna change applied pom.xml lopala

---
+++ Lipi_Kosam_Springboot_ORM/src/main/java/com/amazon/controller/UserDetailsController.java
@@ -0,0 +1,13 @@
+package com.amazon.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.amazon.dto.UserDetailsDTO;
+
+@RestController
+public class UserDetailsController {
+	
+	
+
+}
---
+++ Lipi_Kosam_Springboot_ORM/src/main/java/com/amazon/dto/UserDetailsDTO.java
@@ -0,0 +1,38 @@
+package com.amazon.dto;
+
+public class UserDetailsDTO {
+	
+
+	
+	private Integer id;
+	private String username;
+    private String email;
+    private String adharcardno;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAdharcardno() {
+		return adharcardno;
+	}
+	public void setAdharcardno(String adharcardno) {
+		this.adharcardno = adharcardno;
+	}	
+
+
+
+}
